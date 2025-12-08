f5ee32a4f1f1aa06a21ad2b2ee6a3f493cef78dc
qawse3dr <lawrencemilne38@gmail.com>
2025-12-08 03:56:10 +0000

    fixup common.mk

--- common.mk
+++ common.mk
@@ -8,9 +8,9 @@ NAME=bam-box
 CFLAGS=-std=c++20 -Wall -Werror
 LIBS+=asound spdlog socket
 
-EXTRA_SRCVPATH=$(PROJECT_ROOT)/platform/rpi4/
-EXTRA_LIBVPATH=$(QNX_TARGET)/$(CPUDIR)/usr/local/lib
-EXTRA_INCVPATH=$(QNX_TARGET)/usr/local/include
+EXTRA_SRCVPATH+=$(PROJECT_ROOT)/platform/rpi4/
+EXTRA_LIBVPATH+=$(QNX_TARGET)/$(CPUDIR)/usr/local/lib
+EXTRA_INCVPATH+=$(QNX_TARGET)/usr/local/include
 
 
 include $(MKFILES_ROOT)/qtargets.mk
